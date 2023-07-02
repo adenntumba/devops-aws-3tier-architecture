b1219c487fb2b83c80832f75afabab95f8d10580
Adenn Tumba <adinn.tumba@hotmail.com>
2023-07-02 17:28:51 -0300

    adiciona vpc e providers

---
+++ code/terraform/provider.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+  shared_config_files      = ["~/.aws/config"]
+  shared_credentials_files = ["~/.aws/credentials"]
+  profile                  = "adinn"
+}
+
+terraform {
+  backend "s3" {
+    bucket  = "bucket-my-terraform"
+    key     = "state/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+  }
+}
